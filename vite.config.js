af391b8a91e193c1029f9c380629cf909db4c304
csy23333 <693765678@qq.com>
2021-12-22 17:04:20 +0800

    配置：添加别名、路径简写

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,18 @@
 import { defineConfig } from 'vite'
+import styleImport from 'vite-plugin-style-import';
 import vue from '@vitejs/plugin-vue'
+import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+      "coms": path.resolve(__dirname, './src/components'),
+      "pages": path.resolve(__dirname, './src/pages')
+    },
+    extensions: ['.js','.json']
+  },
   plugins: [vue(),
   styleImport({
     libs: [
